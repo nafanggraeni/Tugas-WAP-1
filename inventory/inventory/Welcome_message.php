b74e352844479abbb90ea6517e9a8c7d30e71844
nafanggraeni <93441467+nafanggraeni@users.noreply.github.com>
2021-12-03 16:25:28 +0700

    Add files via upload

---
+++ inventory/inventory/Welcome_message.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="mt-5 ">Welcome to Mini Main</h1>
+    <p class="lead mb-5">program ini dibuat untuk menginput data barang Mini Main <br/> <small>- nafa</small></p>
+</div> 
+   
+    
